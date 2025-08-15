e7085dfe6a8696b525aba112776b920d1dcbc8c1
Hanniel Maningas <reimaningas@gmai.com>
2025-08-15 16:31:11 +0800

    This is my file in Github

---
+++ code-challenge2.py
@@ -0,0 +1,43 @@
+halaga = eval(input("Ilagay ang halagang ide-deposito: "))
+
+print("Narito ang paghahati gamit ang denominasyon sa Pilipinas:")
+
+# 1000
+libohan = halaga // 1000 
+halaga = halaga % 1000
+print("\t\t\t\t\t\t\t", libohan, "- ₱1000")
+
+# 500
+limandaanan = halaga // 500
+halaga = halaga % 500
+print("\n\t\t\t\t\t\t", limandaanan, "- ₱500")
+
+# 100
+daanan = halaga // 100
+halaga = halaga % 100
+print("\n\t\t\t\t\t", daanan, "- ₱100")
+
+# 50
+limampuan = halaga // 50
+halaga = halaga % 50
+print("\n\t\t\t\t", limampuan, "- ₱50")
+
+# 20
+dalawampuan = halaga // 20
+halaga = halaga % 20
+print("\n\t\t\t", dalawampuan, "- 20")
+
+# 10
+sampuan = halaga // 10
+halaga = halaga % 10
+print("\n\t\t", sampuan, "- ₱10")
+
+# 5
+limahan = halaga // 5
+halaga = halaga % 5
+print("\n\t", limahan, "- ₱5")
+
+# 1
+isahan = halaga // 1
+halaga = halaga % 1
+print("\n", isahan, "- ₱1")
