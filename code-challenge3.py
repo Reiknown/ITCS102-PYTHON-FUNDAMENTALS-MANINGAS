f48821d4d01626d8814981b12c57b062a2a9bbfd
Hanniel Maningas <reimaningas@gmail.com>
2025-08-28 03:48:01 +0800

    This is my 3rd code-challenge

---
+++ code-challenge3.py
@@ -0,0 +1,14 @@
+from getpass import getpass
+# Username & Password
+username = "Maningas"
+password = "Hanniel"
+
+# Login Process
+yourusername = input("HI what is your username --> ")
+yourpassword = getpass("Type your password please --> ")
+
+#Checking
+if yourusername == username and yourpassword == password:
+	print('Access Granted')
+else:
+	print('Acces Denied')
