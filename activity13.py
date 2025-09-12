db7a52ec6861d832c270dbd93129847bacf9073f
Hanniel Maningas <reimaningas@gmail.com>
2025-09-12 11:30:57 +0800

    This is my 2nd VS Code file in Github

---
+++ activity13.py
@@ -0,0 +1,6 @@
+num = 0
+for i in range(1, 11):
+    number = eval(input('Enter a number: '))
+    num += number
+    print('paminta sibuyas bawang betsin isang boteng gin sa lunes babayaran sa halagang:', num)
+print(num)
