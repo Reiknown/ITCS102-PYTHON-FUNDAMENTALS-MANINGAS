c95454f1a7ea4b8aa480cf68f0d401ff0115e325
Hanniel Maningas <reimaningas@gmail.com>
2025-09-12 11:27:02 +0800

    This is my first VS Code file in Github

---
+++ activity12.py
@@ -0,0 +1,7 @@
+# print('hello word')
+# name = input('What is Your Name: ')
+# print('hi', name, 'Have a Nice Day')
+
+#print hello world 10 times
+for i in range(1, 10, 2):
+    print(i, 'hello world')
