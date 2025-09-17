2a8027fce1cb5060677edd0c5d6d92a6b2378af9
Hanniel Maningas <reimaningas@gmail.com>
2025-09-17 12:18:55 +0800

    This is my 9th code-challenge

---
+++ code_challenge9.py
@@ -0,0 +1,8 @@
+print("⏳ COUNTDOWN TIMER SIMULATOR")
+number = int(input("Enter the starting number for countdown: "))
+
+print("\nCountdown started:")
+for i in range(number, 0, -1):
+    print(i)
+
+print("Liftoff!")
