43c9da2daef691270e94f3235a83a694e0e8a78d
Hanniel Maningas <reimaningas@gmail.com>
2025-09-23 12:13:11 +0800

    This is my 11th code-challenge

--- codechallenge11.py
+++ codechallenge11.py
@@ -1,16 +1,16 @@
-for i in range(1, 11, 1):
-    for c in range(10, i, -1):
+for i in range(1, 13, 1):
+    for c in range(12, i, -1):
         print(" ", end=" ")
     for d in range(1, i, 1):
             print("*", end=" ")
-    for e in range(1, i, 1):
+    for e in range(0, i, 1):
             print("*", end=" ")
     print()
-for i in range(1, 11, 1):
-    for c in range(1, i, 1):
+for i in range(1, 13, 1):
+    for c in range(0, i, 1):
         print(" ", end=" ")
-    for d in range(10, i, -1):
+    for d in range(11, i, -1):
         print("*", end=" ")
-    for e in range(10, i, -1):
+    for e in range(12, i, -1):
         print("*", end=" ")
-    print( )
+    print()
