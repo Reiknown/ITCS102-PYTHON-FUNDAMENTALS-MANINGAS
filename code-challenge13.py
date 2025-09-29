fa4533d0cab73de2dd6d293f78236f6979da3311
Hanniel Maningas <reimaningas@gmail.com>
2025-09-29 14:42:47 +0800

    This is my 13th code-challenge

---
+++ code-challenge13.py
@@ -0,0 +1,31 @@
+# DIAMOND
+for i in range(1, 3 + 1):
+    for a in range(4 - i):
+        print(" ", end=" ")
+    for b in range(i + i - 1):
+        print("*", end=" ")
+    print()
+for i in range(2, 0, -1):
+    for c in range(4 - i):
+        print(" ", end=" ")
+    for d in range(i + i - 1):
+        print("*", end=" ")
+    print()
+
+# TRIANGLES
+for e in range(3):
+    for i in range(1, 4 + 1):
+        for f in range(4 - i):
+            print(" ", end=" ")
+        for g in range(i + i - 1):
+            print("*", end=" ")
+        print()
+
+# TRUNK
+for g in range(3):
+    for i in range(2):
+        print(" ", end=" ")
+    print("|", end=" ")
+    print(" ", end=" ")
+    print("|", end=" ")
+    print()
