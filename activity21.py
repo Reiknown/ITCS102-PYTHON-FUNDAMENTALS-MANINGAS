18787c936b76945ccecc5f1fdd7b2c677e9490c4
Hanniel Maningas <reimaningas@gmail.com>
2025-10-15 11:00:13 +0800

    This is my file in Github

---
+++ activity21.py
@@ -0,0 +1,15 @@
+# Washing Machine Logic\
+
+isDirty = True #booleanm variable to initialize the loop
+
+while isDirty == True:
+    confirm = input("Do you wish to continue washing (yes / no) --> ").lower()
+
+    if confirm == 'yes':
+        print("continuing the cycle")
+        continue
+    elif confirm == 'no':
+        print('cycle stops')
+    else:
+        print("invalid choice")
+        continue
