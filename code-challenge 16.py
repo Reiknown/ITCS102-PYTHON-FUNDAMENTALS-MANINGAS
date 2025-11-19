2bc69eab570d3eb65be6eed65dfee1f87d526616
Hanniel Maningas <reimaningas@gmail.com>
2025-11-19 11:34:01 +0800

    UPDATED

--- code-challenge 16.py
+++ code-challenge 16.py
@@ -9,14 +9,14 @@ print("----------------------------------------------\n")
 student_records = {}
 
 while True:
-    print("SELECT FROM THE OPTIONS BELOW \n A- Add Information \n B- Search A Record \n C- Delete A Record \n D- Modiy A Record \n P- Print All Data \n E- Exit")
-
+    print("SELECT FROM THE OPTIONS BELOW \n A- Add Information \n B- Print All Record \n C- Search Student Record \n D- Delete a Student Record \n E- Edit Sutdnet Record \n F- Export Data \n G- Exit")
     choice = input("Your choice            ->   ").lower()
     
     if choice == 'a':
+        os.system('cls')
         print("ADDING STUDENT INFORMATION")
         print(" ----------------------------------------------- ")
-        search_code = input("Key Search For This Student   --> ")
+        student_id = input("Key Search For This Student  use this pattern(course-IDNO) --> ")
 
         first_name = input("Input student first name --> ").upper()
         last_name = input("Input student last name --> ").upper()
@@ -24,36 +24,61 @@ while True:
         email = input("Input student email address --> ")
         isSingle = input("Are your single? (True or False): ")
 
-        student_records = {search_code : [first_name, last_name, course, email, isSingle ]}
+        # adding record to our dictionary
+        student_records[student_id] = [first_name, last_name, course, email, isSingle ]
         print("DATA SAVED")
-
-        os.system('cls')
         continue
 
 # search
     elif choice == 'b':
-        os.system('cls')
-        code = input("Input search code ---> ")
+        print("PRINTING STUDENT RECORD")
 
-        for j in student_records.keys():
-            if code in student_records.keys():
-                print("RECORD FOUND")
-
-                print('Records')
-                print('----------------------------')
-                for i in student_records[code]:
-                    print(i)
-
-            else:
-                    print("NO RECORD FOUND")
-        os.system('cls')
+        for id, record in student_records.items():
+            print(f"STUDENT ID {id} in STUDENT RECORD {record}")
         continue
+
     elif choice == 'c':
-        pass
+        os.system('cls')
+        print("SEARCH STUDENT RECORD ")
+        print(" ============================= ")
+
+        search_id = input("Input ID to search ---> ").lower()
+
+        for id in student_records.keys():
+            if search_id in student_records.keys():
+                print("====================================")
+                print("\n\nRECORD FOUND")
+                print("====================================")
+                # print the record of search stduent
+                for i in student_records[search_id]:
+                    print(f"-- {i}")
+            else:
+                print("====================================")
+                print("\n\nNO RECORD FOUND")
+                print("====================================")
+
         continue
     elif choice == 'd':
-        print("EDITING EXISTING RECORD")
+        os.system('cls')
+        print("DELETE EXISTING RECORD")
+
+        search_id = input("Input ID to search ---> ").lower()
+        if search_id in student_records.keys():
+                print("====================================")
+                print("\n\nRECORD FOUND")
+                print("====================================")
+                # print the record of search stduent
+                for i in student_records[search_id]:
+                    print(f"-- {i}")
+
+                student_records.pop(search_id)
+                print("RECORD DELETED")
+        else:
+                print("====================================")
+                print("\n\nNO RECORD FOUND")
+                print("====================================")
         continue
+
     elif choice == 'p':
         for i,j in student_records.items():
             print(f"CODE - {i} STORED INFORMATION ---> {j}")
