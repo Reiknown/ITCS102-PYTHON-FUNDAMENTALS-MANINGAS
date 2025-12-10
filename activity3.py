6e64ef6be92c927259a983911c098165b5c718ae
Hanniel Maningas <reimaningas@gmail.com>
2025-12-10 19:06:33 +0800

    Activity #3

--- activity3.py
+++
@@ -1,3 +0,0 @@
-print("Happy:)\t\tMonday")
-print("\n\tBSIT1A \n from \n\tDLL")
-print("The \n\tQuick \n\t\tBrown \n\t\t\tFox \n\t\t\t\tJumps \n\t\t\t\t\tOver \n\t\t\t\t\t\tThe \n\t\t\t\t\t\t\tLazy \n\t\t\t\t\t\t\t\tDog")
