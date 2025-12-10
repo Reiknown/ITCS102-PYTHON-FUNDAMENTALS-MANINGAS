20a36ba72223a9e38c43753f268aaaeea4b54c26
Hanniel Maningas <reimaningas@gmail.com>
2025-12-11 00:52:10 +0800

    This is my 1st code challenge file in GitHub

--- Code_Challenge01.py
+++ Code_Challenge01.py
@@ -1,5 +1,3 @@
 name = input("State your first & last name, please...")
 
 print("\n\n \t\t\t\t\t♦ \n\n\n \t\t\t\t♦\t\t♦ \n\n\n \t\t\t♦\t\t\t\t♦ \n\n\n \t\t♦\t\t\tHI\t\t\t♦ \n\n\n \t♦\t\t\t" , name , "\t\t\t♦ \n\n\n \t\t♦\t\t\t\t\t\t♦ \n\n\n \t\t\t♦\t\t\t\t♦ \n\n\n \t\t\t\t♦\t\t♦ \n\n\n \t\t\t\t\t♦")
-
-
